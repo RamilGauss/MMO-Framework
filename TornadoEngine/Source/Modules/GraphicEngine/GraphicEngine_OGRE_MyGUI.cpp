5e59bc48051195002651bb5edffe6882071dae47
Ramil <ramil2085@mail.ru>
2015-02-01 20:26:42 +0300

    Adding project GraphicEngine
    Then seems need description of interface GE.

---
+++ TornadoEngine/Source/Modules/GraphicEngine/GraphicEngine_OGRE_MyGUI.cpp
@@ -0,0 +1,24 @@
+/*
+Author: Gudakov Ramil Sergeevich a.k.a. Gauss 
+Гудаков Рамиль Сергеевич 
+Contacts: [ramil2085@mail.ru, ramil2085@gmail.com]
+See for more information License.h.
+*/
+
+#include "GraphicEngine_OGRE_MyGUI.h"
+
+TGraphicEngine_OGRE_MyGUI::TGraphicEngine_OGRE_MyGUI()
+{
+
+}
+//---------------------------------------------------------------------
+TGraphicEngine_OGRE_MyGUI::~TGraphicEngine_OGRE_MyGUI()
+{
+
+}
+//---------------------------------------------------------------------
+void TGraphicEngine_OGRE_MyGUI::Work()
+{
+
+}
+//---------------------------------------------------------------------
---
+++ TornadoEngine/Source/Modules/GraphicEngine/GraphicEngine_OGRE_MyGUI.h
@@ -0,0 +1,25 @@
+/*
+Author: Gudakov Ramil Sergeevich a.k.a. Gauss 
+Гудаков Рамиль Сергеевич 
+Contacts: [ramil2085@mail.ru, ramil2085@gmail.com]
+See for more information License.h.
+*/
+
+#ifndef GraphicEngine_OGRE_MyGUIH
+#define GraphicEngine_OGRE_MyGUIH
+
+#include "TypeDef.h"
+
+class DllExport TGraphicEngine_OGRE_MyGUI
+{
+public:
+  TGraphicEngine_OGRE_MyGUI();
+  virtual ~TGraphicEngine_OGRE_MyGUI();
+
+	virtual void Work();
+protected:
+
+};
+
+
+#endif
