d488a10bc6bc99bd68921b74c16a7d8d68cd7d84
RamilGauss <ramil2085@mail.ru>
2017-01-04 15:32:19 +0300

    Change rules how compile (Windows)

--- TornadoEngine/Source/openSolution.bat
+++ TornadoEngine/Source/openSolution.bat
@@ -1,5 +1,4 @@
 set QMAKESPEC=win32-msvc2008
-set QTDIR=C:\qt\4.8.5
-set PATH=%PATH%;%QTDIR%\bin;
-rem set PATH=e:\MyProjects\Dependency\Bin\ - on My comp, your comp - your rules
+set PATH += %DEP_PATH_MMO_FRAMEWORK%\bin;
+
 start MMOFramework.sln
