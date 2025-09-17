5bede253e3d8ddd07681733c530828e0ac623f37
RamilGauss <ramil2085@mail.ru>
2025-09-17 12:43:44 +0300

    Added aloowed options to tool.

--- Source/Tools/ContainerCodeGenerator/Sources/main.cpp
+++ Source/Tools/ContainerCodeGenerator/Sources/main.cpp
@@ -15,32 +15,17 @@ See for more information LICENSE.md.
 
 namespace po = boost::program_options;
 
-void PrintUsage(char* argv[])
-{
-    printf("usage: %s --type \"core\"|\"project\" --task_file filePath --common_options json_content", argv[0]);
-}
-
 int main(int argc, char* argv[])
 {
-    // const char* sLocale = setlocale(LC_CTYPE, "");
-    // sLocale = setlocale(LC_CTYPE, sLocale);
-    // if (sLocale == nullptr) {
-    //     BL_FIX_BUG();
-    //     return -1;
-    // }
-
-    // if (argc != 3) {
-    //     PrintUsage(argv);
-    //     return -2;
-    // }
-
-    // std::string generatorType = argv[1];
-    // std::string filePath = argv[2];
-
-    //###
+    const char* sLocale = setlocale(LC_CTYPE, "");
+    sLocale = setlocale(LC_CTYPE, sLocale);
+    if (sLocale == nullptr) {
+        BL_FIX_BUG();
+        return -1;
+    }
     po::options_description desc("Allowed options");
     desc.add_options()
-        ("help,h", po::value<std::string>(), "Help")
+        ("help,h", "Help")
         ("type,t", po::value<std::string>(), "type of task")
         ("task_file,f", po::value<std::string>(), "path to task file")
         ("common_options,c", po::value<std::string>(), "content of common options in json format");
@@ -51,32 +36,37 @@ int main(int argc, char* argv[])
         po::notify(vm);
     } catch (const po::error& e) {
         std::cerr << "Error: " << e.what() << std::endl;
+        std::cout << "Example:\n" << desc << std::endl;
         return 1;
     }
-
+    if (vm.count("help")) {
+        std::cout << desc << std::endl;
+        return -2;
+    }
+    std::string type;
     if (vm.count("type")) {
-        std::cout << "type: " << vm["type"].as<std::string>() << std::endl;
+        type = vm["type"].as<std::string>();
     }
+    std::string taskFile;
     if (vm.count("task_file")) {
-        std::cout << "task_file: " << vm["task_file"].as<std::string>() << std::endl;
+        taskFile = vm["task_file"].as<std::string>();
     }
+    std::string commonOptions;
     if (vm.count("common_options")) {
-        std::cout << "common_options: " << vm["common_options"].as<std::string>() << std::endl;
+        commonOptions = vm["common_options"].as<std::string>();
     }
-    //###
-
-
-    // nsContainerCodeGenerator::TContainerCodeGenerator* ccg = nullptr;
-    // if (generatorType == "core") {
-    //     ccg = new nsContainerCodeGenerator::TCoreContainerCodeGenerator();
-    // } else if (generatorType == "project") {
-    //     ccg = new nsContainerCodeGenerator::TProjectContainerCodeGenerator();
-    // } else {
-    //     PrintUsage(argv);
-    //     return -3;
-    // }
-    // auto result = static_cast<int>(ccg->Generate(filePath));
-    // delete ccg;
-
-    // return result;
+    if (type.empty() || taskFile.empty() || commonOptions.empty()) {
+        std::cout << desc << std::endl;
+        return -3;
+    }
+    std::shared_ptr<nsContainerCodeGenerator::TContainerCodeGenerator> ccg;
+    if (type == "core") {
+        ccg = std::make_shared<nsContainerCodeGenerator::TCoreContainerCodeGenerator>();
+    } else if (type == "project") {
+        ccg = std::make_shared<nsContainerCodeGenerator::TProjectContainerCodeGenerator>();
+    } else {
+        std::cout << desc << std::endl;
+        return -3;
+    }
+    return static_cast<int>(ccg->Generate(taskFile));
 }
